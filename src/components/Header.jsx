41bc8bba56745ccb04e16c01de6a48e365af6a76
shekinahmanyi <shekinahmanyi@gmail.com>
2022-10-28 05:37:00 +0100

    feat:added styles to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Header( {text}) {
+function Header( {text,bgColor,textColor}) {
+
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container"></div>
         <h2>{text}</h2>
     </header>
@@ -12,6 +18,8 @@ function Header( {text}) {
 
 Header.defaultProps = {
     text:'Feedback UI',
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor:'#ff6a95',
 }
 
 Header.propTypes = {
