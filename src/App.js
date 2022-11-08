c56fe81cbfd30dd3b85bf2a16d4a15ea2ed75911
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-08 07:46:04 +0100

    feat:added the feedback component to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
     <Header/>
     <div className="container">
-   <h1>My App</h1>
-    </div>
+      <FeedbackItem/>
+      </div>
     </>
   );
 }
