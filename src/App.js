9240e89bada846277bafc6436a1c1faf36d54435
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-08 09:17:37 +0100

    display the feedbacklist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
     <Header/>
     <div className="container">
-      <FeedbackItem/>
+      <FeedbackList feedback={feedback}/>
       </div>
     </>
   );
