cff072c49c585873b44f4900f5573f0deec2bcad
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-08 07:47:56 +0100

    feat:created the feedbackitem list component

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FeedbackItem() {
+  return (
+    <div className='card'>
+      <div className="num-display"></div>
+      <div className="text-display">
+        This is an example of a feedback item
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
