20772572ac19564a9c99b1c43cdf0744f04f5120
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-23 05:48:13 +0100

    imported the added files to the app.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
-
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
 
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -20,6 +20,7 @@ function App() {
     <>
     <Header/>
     <div className="container">
+      <FeedbackForm/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback}
       handleDelete={deleteFeedback}/>
