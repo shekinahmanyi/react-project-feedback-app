1a9152ebc5b59b25158795bf7835db286de25133
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-23 06:12:35 +0100

    added the various states of the messages

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,7 +6,20 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const[btnDisabled, setBtnDisabled] = useState(true)
+  const [message,setMessage] = useState('')
+
   const handleTextChange = (e) => {
+   if(text === ''){
+    setBtnDisabled(true)
+    setMessage(null)
+   }else if(text !== '' && text.trim().length <= 10 ){
+    setMessage('Text must alleast be 10 characters')
+    setBtnDisabled(true)
+   }else {
+    setMessage(null)
+    setBtnDisabled(false)
+   }
     setText(e.target.value);
   };
 
@@ -22,8 +35,9 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type='submit' version='secondary'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+    {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
