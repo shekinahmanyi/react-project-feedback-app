2d7ba7a155d14076d5fa8aa3793787251c52e8ab
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-23 05:45:52 +0100

    created a feedback form to collect feedback from users

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import React from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/*@todo - rating select your component */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type='submit' version='secondary'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
