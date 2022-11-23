b4d6fd9fabb6240408714f47dec6d0f3be015a96
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-23 04:47:19 +0100

    added the handledelete prop

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,23 @@
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
 import React from "react";
 import Card from "./shared/Card";
-import { useState } from "react";
 
-function FeedbackItem({item}) {
+function FeedbackItem({item,handleDelete}) {
   
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color='purple'/>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
+}
+
+FeedbackItem.propTypes = {
+  item:PropTypes.object.isRequired,
 }
 
 export default FeedbackItem;
