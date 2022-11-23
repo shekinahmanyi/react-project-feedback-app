6d39f2593e7b192d14383c662222977cc48c7e27
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-23 04:50:41 +0100

    changed the name of the heading text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header( {text,bgColor,textColor}) {
 }
 
 Header.defaultProps = {
-    text:'Feedback UI',
+    text:'Feedback App',
     bgColor:'rgba(0,0,0,0.4)',
     textColor:'#ff6a95',
 }
