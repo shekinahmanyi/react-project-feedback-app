e4cbe2f3b2fbca1720574be037377dd604d6245d
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-24 11:09:28 +0100

    added the about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+import { Link } from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card>
+     <div className="about">
+        <h1>About this Project</h1>
+        <p>This is a React app to leave feedback for my product and services if You Like them</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+            <Link to="/">Back to Home</Link>
+        </p>
+     </div>
+    </Card>
+  )
+}
+
+export default AboutPage
