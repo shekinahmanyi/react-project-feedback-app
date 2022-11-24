a2869da0695f20ab31268a8f95a554124ffc12de
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-24 09:03:44 +0100

    imported the added file components to the apjs file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
@@ -6,24 +7,25 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 function App() {
-
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback,...feedback])
+  };
 
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')){
-      setFeedback(feedback.filter((item) => item.id !== id))
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-    
-  }
+  };
 
   return (
     <>
-    <Header/>
-    <div className="container">
-      <FeedbackForm/>
-      <FeedbackStats feedback={feedback}/>
-      <FeedbackList feedback={feedback}
-      handleDelete={deleteFeedback}/>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
