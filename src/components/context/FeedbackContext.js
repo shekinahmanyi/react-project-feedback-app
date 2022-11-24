ff900f929a283cb41648dd5d2dff18b104d23cdd
shekinahmanyi <shekinahmanyi@gmail.com>
2022-11-24 12:36:56 +0100

    transfer all the functions to the context file

---
+++ src/components/context/FeedbackContext.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { createContext,useState } from 'react'
+import { v4 as uuidv4 } from "uuid";
+
+const FeedbackContext = createContext()
+
+
+export const FeedbackProvider = ({children}) => {
+    const [feedback,setFeedback] = useState([
+        {
+            id: 1,
+            text:'This is feedback item 1',
+            rating:10
+        },
+        {
+            id: 2,
+            text:'This is feedback item 2',
+            rating:10
+        },
+        {
+            id: 2,
+            text:'This is feedback item 2',
+            rating:10
+        }
+    ])
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+      };
+
+    const deleteFeedback = (id) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+          setFeedback(feedback.filter((item) => item.id !== id));
+        }
+      };
+    
+
+
+    return  ( <FeedbackContext.Provider
+     value ={{
+      feedback,
+      deleteFeedback,
+      addFeedback,
+    }}
+    >
+        {children}
+    </FeedbackContext.Provider>
+    )
+}
+
+export default FeedbackContext
