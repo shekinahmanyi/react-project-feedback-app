872534fad12555d92c5dd088e3c961bf17533d8f
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-20 11:09:03 +0100

    setting up a proxy

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -20,7 +20,7 @@ export const FeedbackProvider = ({children}) => {
 
 //fetch feedback
 const fetchFeedback = async () => {
-  const response = await fetch(`http://localhost:5000/feedback?_sort=id$_order=desc`)
+  const response = await fetch(`/feedback?_sort=id$_order=desc`)
   const data = await response.json()
 
 setFeedback(data);
