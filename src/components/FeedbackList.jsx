36fea0e7f0a13afc4b71800e190ee102c8522029
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-20 10:43:36 +0100

    added the useState isloading

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -5,14 +5,14 @@ import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "./context/FeedbackContext";
 
 function FeedbackList() {
- const { feedback } =useContext(FeedbackContext)
+ const { feedback, isLoading } =useContext(FeedbackContext)
 
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
 
-  return (
+  return isLoading ? <h3>Loading....</h3> : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -27,7 +27,7 @@ function FeedbackList() {
         ))}
       </AnimatePresence>
     </div>
-  );
+  )
 }
 
 
