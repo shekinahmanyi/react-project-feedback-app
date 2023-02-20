66c675abfd968c615ed08d4f2349eca717e86c43
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-20 18:05:27 +0100

    Update and Delete from JSON Server

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -45,17 +45,30 @@ setIsLoading(false)
 
 // delete feedback
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure you want to delete?")) {
+
+          await fetch(`/feedback/${id}`,{ method:'DELETE'})
+
           setFeedback(feedback.filter((item) => item.id !== id));
         }
       };
 
 
 //update feedback item 
-const updateFeedback = (id, updItem) => {
+const updateFeedback = async (id, updItem) => {
+  const response = await fetch(`/feedback/${id}`,{
+    method:'PUT',
+    headers: {
+      'Content-Type':'application/json',
+    },
+    body: JSON.stringify(updItem)
+  })
+
+const data = await response.json()
+
   setFeedback(
-    feedback.map((item) => (item.id === id ? {...item, ...updItem} : item))
+    feedback.map((item) => (item.id === id ? {...item, ...data} : item))
   )
 }
 
